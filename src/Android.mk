008a0f3b19e0aaa93cb8f4ba32fcd18f3344b3bb
Sungwon Jo <doriya@nexell.co.kr>
2017-06-29 16:59:31 +0900

    nx-video-api: add Android.mk for build.
    This patch add Android.mk for build.
    
    Change-Id: I06a37b2db2ebfba0c0e26b348b5a57efd35febd0
    Signed-off-by: Sungwon Jo <doriya@nexell.co.kr>

---
+++ src/Android.mk
@@ -0,0 +1,59 @@
+LOCAL_PATH := $(call my-dir)
+include $(CLEAR_VARS)
+
+#
+#	Compile Flags
+#
+USE_DRM_ALLOCATOR := false
+
+#
+#	Compile Options
+#
+ifeq ($(USE_DRM_ALLOCATOR),true)
+LOCAL_CFLAGS += -DARM64=1 -DUSE_DRM_ALLOCATOR=1
+else
+LOCAL_CFLAGS += -DPLATFORM_SDK_VERSION=$(PLATFORM_SDK_VERSION) -DUSE_DRM_ALLOCATOR=0
+endif
+
+LOCAL_C_INCLUDES := \
+	$(LOCAL_PATH)/include		\
+	system/core/include			\
+	system/media/core/include 	\
+	system/core/libsync/include \
+	frameworks/native/include 	\
+	frameworks/av/include 		\
+	hardware/nexell/s5pxx18/gralloc
+
+LOCAL_SHARED_LIBRARIES :=	\
+	liblog 		\
+	libcutils	\
+	libhardware
+
+ifeq ($(USE_DRM_ALLOCATOR),true)
+LOCAL_SRC_FILES := \
+	nx_video_alloc_drm.c \
+	nx_video_enc.c \
+	nx_video_dec.c
+else
+LOCAL_SRC_FILES := \
+	nx_video_alloc_ion.cpp \
+	nx_video_enc.c \
+	nx_video_dec.c
+endif
+
+# We need this because the current asm generates the following link error:
+# requires unsupported dynamic reloc R_ARM_REL32; recompile with -fPIC
+# Bug: 16853291
+LOCAL_LDFLAGS := -Wl,-Bsymbolic
+LOCAL_LDFLAGS += \
+	-L$(LINUX_LIBS)
+
+LOCAL_32_BIT_ONLY := true
+
+LOCAL_MODULE := libnx_video_api
+LOCAL_MODULE_PATH := $(LOCAL_PATH)
+
+LOCAL_MODULE_TAGS := optional
+
+include $(BUILD_SHARED_LIBRARY)
+
